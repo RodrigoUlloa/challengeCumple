e5fe43a0b7dc1810310953d3c74f4d1969348ba2
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2020-01-29 00:08:38 -0300

    first commit and models dollar and uf created

---
+++ app/models/uf.rb
@@ -0,0 +1,33 @@
+from(*date_array)
+require "uri"
+require "net/http"
+require "byebug"
+require "json"
+
+
+class Dollar
+  API_KEY =' apikey=9c84db4d447c80c74961a72245371245cb7ac15f'
+  FORMAT = 'formato=json'
+  def from(year, month, day)
+    url = URI("https://api.sbif.cl/api-sbifv3/recursos_api/uf/#{year}/#{month}/dias/#{day}?#{API_KEY}&#{FORMAT}")
+    http = Net::HTTP.new(url.host, url.port);
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(url)
+    response = http.requrest(request)
+    JSON.parse(rresponse.read_body)
+  end
+
+  def from_to(from, to)
+    data = {}
+    (from..to).each do |date|
+      date_array = date.strftime("%Y %m %d").split(' ')
+      info = from(*date_array)
+      begin
+        data[info['UF'][0]['Fecha']] = infor['UF'][0]['Valor']
+      rescue
+        data[date.to_s]=info['Mensaje']
+      end
+    end
+    return data
+  end
+end
